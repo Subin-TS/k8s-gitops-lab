52df64c7f36061cfab551bbc24c3aa8e55cbb27b
Subin-TS <subin4bsl@gmail.com>
2026-09-22 11:56:10 +0000

    Release application version 2

--- app/app.py
+++ app/app.py
@@ -7,13 +7,15 @@ def home():
     return {
         "application": "k8s-gitops-lab",
         "message": "Hello from our DevOps application!",
+        "version": "v2",
         "status": "running"
     }
 
 @app.route("/health")
 def health():
     return {
-        "status": "healthy"
+        "status": "healthy",
+        "version": "v2"
     }
 
 if __name__ == "__main__":
